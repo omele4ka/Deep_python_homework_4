be3cff539a178df6d83783070861648a6564493e
omele4ka <omele4ka@inbox.ru>
2023-07-27 15:43:59 -0500

    task 2 is solved

---
+++ task_2.py
@@ -0,0 +1,18 @@
+# Напишите функцию принимающую на вход только ключевые параметры
+# и возвращающую словарь, где ключ — значение переданного аргумента,
+# а значение — имя аргумента. Если ключ не хешируем, используйте его строковое представление.
+# Пример: rev_kwargs(res=1, reverse=[1, 2, 3]) -> {1: 'res', '[1, 2, 3]': 'reverse'}
+
+def key_args_dict(**kwags) -> dict:
+    result_dict = {}
+    for key, value in kwags.items():
+        if isinstance(value, (list, set, dict)):
+            value_str = str(value)
+            result_dict[value_str] = key
+        else:
+            result_dict[value] = key
+
+    return result_dict
+
+my_dict = key_args_dict(res=1, reverse=[1, 2, 3])
+print(my_dict)
